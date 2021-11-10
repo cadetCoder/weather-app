10f21a597e5ee2459da225fe997a2a37ac478e68
Darrel Toledo <darreltoledo@icloud.com>
2021-11-10 22:06:51 +0000

    DOM description element

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //SELECT ELEMENTS
 const iconElement = document.querySelector(".weather-icon");
 const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
 
 window.addEventListener('load', ()=> {
     let long;
