0f043d1340295c5a82622d16a9c6bea618b31b77
Darrel Toledo <darreltoledo@icloud.com>
2021-11-10 22:07:09 +0000

    DOM location element

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const iconElement = document.querySelector(".weather-icon");
 const tempElement = document.querySelector(".temperature-value p");
 const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
 
 window.addEventListener('load', ()=> {
     let long;
