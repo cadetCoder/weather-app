9b973bde213271deabed0b59e29c928623e144fc
Darrel Toledo <darreltoledo@icloud.com>
2021-11-10 22:07:20 +0000

    DOM notification element

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const iconElement = document.querySelector(".weather-icon");
 const tempElement = document.querySelector(".temperature-value p");
 const descElement = document.querySelector(".temperature-description p");
 const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
 
 window.addEventListener('load', ()=> {
     let long;
