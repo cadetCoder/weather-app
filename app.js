22bb61ccce966d731a8e74efd3a7afbdfaca37ed
Darrel Toledo <darreltoledo@icloud.com>
2021-11-13 22:06:07 +0000

    setting up js

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+//https://home.openweathermap.org/api_keys
+// api key : cd4738e1a11c871739d95f5c440bd48d
+
 //SELECT ELEMENTS
 const iconElement = document.querySelector(".weather-icon");
 const tempElement = document.querySelector(".temperature-value p");
 const descElement = document.querySelector(".temperature-description p");
 const locationElement = document.querySelector(".location p");
 const notificationElement = document.querySelector(".notification");
+
+// App data
+const weather = {
+
+};
+
+weather.temperature = {
+  unit: "celsius"
+}
+
+// APP CONST AND VAR
+const KELVIN = 273;
 
 window.addEventListener('load', ()=> {
     let long;
