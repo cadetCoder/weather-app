5401341ef43dc1165487292f84eb561353a5c4ce
Darrel Toledo <darreltoledo@icloud.com>
2021-11-19 22:34:32 +0000

    debug

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ weather.temperature = {
 // APP CONST AND VAR
 const KELVIN = 273;
 // API Key
-const key = "cd4738e1a11c871739d95f5c440bd48d";
+const key = "82005d27a116c2880c8f0fcb866998a0";
 
 //CHECK IF BROWSER SUPPORTS GEOLOCATION
 if('geolocation' in navigator) {
@@ -46,7 +46,7 @@ function showError(error) {
 
 // GET WEATHER DATA FROM API PROVIDER
 function getWeather(latitude, longitude){
-  let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&api=${key}`;
+  let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
   fetch(api)
     .then(function(response){
       let data = response.json();
