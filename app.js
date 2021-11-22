c50bb91b4a773d6592a69327667df05ff27336c9
Darrel Toledo <darreltoledo@icloud.com>
2021-11-22 21:18:24 +0000

    f to c click eventlistener

--- app.js
+++ app.js
@@ -79,15 +79,18 @@ function celsiusToFahrenheit(temperature){
   return (temperature * 9/5) + 32;
 }
 
-//WHEN THE USER CLICKS ON THE TEMPERATURE ELEMENT
+// WHEN THE USER CLICKS ON THE TEMPERATURE ELEMENET
 tempElement.addEventListener("click", function(){
   if(weather.temperature.value === undefined) return;
-
-    if(weather.temperature.unit == "celcius"){
+  
+  if(weather.temperature.unit == "celsius"){
       let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
       fahrenheit = Math.floor(fahrenheit);
-
+      
       tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
       weather.temperature.unit = "fahrenheit";
-    }
-})
+  }else{
+      tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+      weather.temperature.unit = "celsius"
+  }
+});
